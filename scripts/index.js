5ea0be288957cfe38d6c40d169e20d30a2d13be2
Cristiana Barboza <50346228+cbarboza2011@users.noreply.github.com>
2020-12-31 14:37:49 -0600

    init

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('.list').hide();
+  $('.menu-button').on('click', function () {
+    $('.list').slideToggle();
+  });
+  $('.list a').on('click', function () {
+    $('.list').slideUp();
+  });
+
+
+  $('#slideshow').slick({
+    dots: false,
+    infinite: true,
+  	speed: 1000,
+  	slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+
+
+});
